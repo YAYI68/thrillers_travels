8c5c195e1adbe533832ab25bc714c11dc88ea55b
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-08-05 16:48:34 +0100

    mobile toggle

--- src/components/home/header/MobileHeader.tsx
+++ src/components/home/header/MobileHeader.tsx
@@ -4,6 +4,27 @@ import React, { useState } from "react";
 import { HiBars3BottomRight } from "react-icons/hi2";
 import { LiaTimesSolid } from "react-icons/lia";
 import ThrillerImg from "../../../../public/images/thriller_travel.webp";
+import Link from "next/link";
+
+type MobileLinkProps = {
+  className?: string;
+  name: string;
+  url: string;
+};
+
+const MobileLink = (props: MobileLinkProps) => {
+  const { className, name, url } = props;
+  return (
+    <li className="">
+      <Link
+        href={url}
+        className={`${className} w-full h-full text-blue-500 hover:bg-blue-400 hover:text-white block p-2 rounded`}
+      >
+        {name}
+      </Link>
+    </li>
+  );
+};
 
 type Props = {};
 
@@ -30,11 +51,36 @@ const MobileHeader = (props: Props) => {
       </div>
       {/* header Slide In */}
       <div
-        className={`w-full p-2 absolute top-[100%] left-0 transition-[transform] ${
+        className={`w-full p-2 bg-[rgba(0,0,0,.2)] absolute top-[100%] left-0 transition-[transform] duration-300 border rounded ${
           slideIn ? "translate-x-0" : "translate-x-[-100%]"
-        } bg-black h-[50vh]`}
+        }  `}
       >
-        <div className="bg-yellow-500 w-full h-full "></div>
+        <div className=" w-full h-full flex flex-col gap-4 ">
+          <ul className="w-full flex flex-col gap-2">
+            <MobileLink name="Home" url="" />
+            <MobileLink name="Flights" url="" />
+            <MobileLink name="About" url="" />
+            <MobileLink name="Contact" url="" />
+          </ul>
+          <ul className="w-full flex flex-col gap-2">
+            <li>
+              <Link
+                href={""}
+                className=" w-full h-full border hover:bg-blue-400 border-blue-400 text-center font-medium hover: hover:text-white block p-2 rounded"
+              >
+                login
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={""}
+                className=" w-full h-full border bg-black border-black text-center font-medium hover: text-white block p-2 rounded"
+              >
+                signup
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
